fa899012fbfd3babed686ac43944d4be8608530e
hassan-jamshaid10 <hjamshaid81@gmail.com>
2024-11-25 22:52:24 +0500

    Fixed main

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-//import {store} from '../store.js'
-import './index.css'
-import App from './App.jsx'
-//import { Provider } from 'react-redux'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import store from '../store.js'; // Correct default import
+import './index.css';
+import App from './App.jsx';
+import { Provider } from 'react-redux';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/* <Provider store = {store}>
-    <App />
-    </Provider>  */}
-    <App />
-  </StrictMode>,
-)
-
-//for pull
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </StrictMode>
+);
